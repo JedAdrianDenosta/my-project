740df44491cb3157aba45212330ab29a28a14246
JedAdrianDenosta <Jadriandenosta@wvsu.edu.ph>
2022-04-30 20:43:51 +0800

    test

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <div className="min-h-screen flex flex-col ">
+      <nav></nav>
+      <main className="flex-grow flex flex-col h-full">
+        <div className="h-full flex flex-col flex-grow items-center justify-center">
+          <h1>Hello, Next.js</h1>
+          <p>Goodl luck</p>
+        </div>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default Navbar;
